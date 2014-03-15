851e4b8af116f57e5a018ee4e7880d82f493e0d8
fogus <mefogus@gmail.com>
2014-03-14 23:41:23 -0400

    CLJS 101 specific stuff

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-; Copyright (c) 2012 Fogus and Relevance Inc. All rights reserved.  The
+; Copyright (c) 2014 Fogus. All rights reserved.  The
 ; use and distribution terms for this software are covered by the Eclipse
 ; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ; which can be found in the file COPYING the root of this
@@ -6,8 +6,8 @@
 ; agreeing to be bound by the terms of this license.  You must not
 ; remove this notice, or any other, from this software.
 
-(defproject himera "0.1.0-SNAPSHOT"
-  :description "ClojureScript compiler service."
+(defproject himera "1.0.0"
+  :description "ClojureScript 101"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.0.2"]
                  [compojure "1.0.1"]
@@ -15,8 +15,7 @@
                  [ring-clj-params "0.1.0"]
                  [org.clojure/clojurescript "0.0-1847"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
-  :dev-dependencies [[jline "0.9.94"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]]
+  :dev-dependencies [[jline "0.9.94"]]
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"],
                 :compiler
@@ -25,10 +24,7 @@
                  :output-to "resources/public/js/repl.js",
                  :optimizations :simple},
                 :jar true}]}  
-  :jvm-opts ["-Djava.security.policy=heroku.policy" "-Xmx80M"]
   :source-paths ["src/clj"]
   :main himera.server.app
-  :min-lein-version "2.0.0"
-  ;;:hooks [leiningen.js]
-  )
+  :min-lein-version "2.0.0")
 
